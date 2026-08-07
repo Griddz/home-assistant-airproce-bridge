d8a9ec71aacd200ed856ec7a5cf1cb2e837579ac
Griddz <87234855+Griddz@users.noreply.github.com>
2026-08-07 14:27:04 +0800

    Bump AirProce version to 0.2.1

--- custom_components/airproce_bridge/const.py
+++ custom_components/airproce_bridge/const.py
@@ -6,7 +6,7 @@ from homeassistant.const import Platform
 
 DOMAIN = "airproce_bridge"
 NAME = "AirProce"
-VERSION = "0.2.0"
+VERSION = "0.2.1"
 
 CONF_DEVICE_NAME = "device_name"
 CONF_DEVICE_MODEL = "device_model"
